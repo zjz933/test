a812ae82b21fb02631d26b80430bf6e3e901d913
zjz933 <85262342@qq.com>
2017-03-14 15:30:42 +0800

    local branche

--- src/com/trendmicro/test/test.java
+++ src/com/trendmicro/test/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is branche of dev");
+		System.out.println("this is local branche of dev");
 
 	}
 
